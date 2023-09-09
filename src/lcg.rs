e2611714b0cf1e71d0323fb2e2145df784a3b6da
fesmjke <fatalerror.sm@gmail.com>
2023-09-09 22:53:31 +0300

    feat: added implementation of linear congruential generator

---
+++ src/lcg.rs
@@ -0,0 +1,22 @@
+pub struct LCG {
+    modulus: i32,
+    multiplier: i32,
+    increment: i32,
+    seed: i32
+}
+
+impl LCG {
+    pub fn new(modulus: i32, multiplier: i32, increment: i32, seed: i32) -> Self {
+        Self { modulus, multiplier, increment, seed }
+    }
+}
+
+impl Iterator for LCG {
+    type Item = i32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.seed = (self.multiplier * self.seed + self.increment) % self.modulus;
+
+        Some(self.seed)
+    }
+}

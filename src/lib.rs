19cff67ade741502ed0be7a311d886108682dda8
fesmjke <fatalerror.sm@gmail.com>
2023-09-09 22:54:34 +0300

    feat: added tests and modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,50 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod lcg;
+mod utils;
 
 #[cfg(test)]
-mod tests {
+mod core {
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    mod lcg {
+        use crate::lcg::LCG;
+
+        #[test]
+        fn lcg_lecture() {
+            let modulus = 1 << 5; // 32
+            let multiplier = 7;
+            let increment = 0;
+            let seed = 1;
+
+            let mut lcg = LCG::new(modulus, multiplier, increment, seed);
+            let mut nums = Vec::<i32>::with_capacity(32);
+
+            for _ in 0..5 {
+                let num = lcg.next().expect("Failed generate next number!");
+
+                nums.push(num);
+            }
+
+            assert_eq!(vec![7, 17, 23, 1, 7], nums);
+        }
+        #[test]
+        fn lcg_own() {
+            let modulus = (1 << 11) - 1; // 2047
+            let multiplier = 3_i32.pow(5); // 243
+            let increment = 1;
+            let seed = 4;
+
+            let mut lcg = LCG::new(modulus, multiplier, increment, seed);
+
+            let mut nums = Vec::<i32>::with_capacity(1000);
+            for _ in 0..1000 {
+                let num = lcg.next().expect("Failed generate next number!");
+
+                nums.push(num);
+            }
+
+            println!("{:?}", nums);
+
+            assert_eq!(nums.len(), 1000);
+        }
     }
 }

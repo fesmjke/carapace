283d2cc8b4c2cd69c14174063810a2847c0cb0c1
fesmjke <fatalerror.sm@gmail.com>
2023-09-11 22:19:25 +0300

    feat: added unique util fn

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,8 @@
 use std::cmp::Eq;
+use std::collections::HashSet;
 use std::hash::Hash;
 
 pub fn unique<T: Eq + Hash>(sequence: &Vec<T>) -> usize {
     // TODO: implement a unique number of elements in vector
-    todo!()
+    sequence.into_iter().map(|x| x).collect::<HashSet<_>>().len()
 }

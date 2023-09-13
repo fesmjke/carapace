217b25b1159028d5ec9d10e2ff33c342ff7bddc7
fesmjke <fatalerror.sm@gmail.com>
2023-09-13 20:45:30 +0300

    fix:removed todo commend and formatted

--- src/utils.rs
+++ src/utils.rs
@@ -3,6 +3,9 @@ use std::collections::HashSet;
 use std::hash::Hash;
 
 pub fn unique<T: Eq + Hash>(sequence: &Vec<T>) -> usize {
-    // TODO: implement a unique number of elements in vector
-    sequence.into_iter().map(|x| x).collect::<HashSet<_>>().len()
+    sequence
+        .into_iter()
+        .map(|x| x)
+        .collect::<HashSet<_>>()
+        .len()
 }

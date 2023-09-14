ce681c21d6e54ef00bb5f7472500db15e01f3923
fesmjke <fatalerror.sm@gmail.com>
2023-09-14 15:50:18 +0300

    fix: fixed lcg testing (replaced i32 to u64)

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@ mod core {
             let seed = 1;
 
             let mut lcg = LCG::new(modulus, multiplier, increment, seed);
-            let mut nums = Vec::<i32>::with_capacity(32);
+            let mut nums = Vec::<u64>::with_capacity(32);
 
             for _ in 0..5 {
                 let num = lcg.next().expect("Failed generate next number!");
@@ -30,13 +30,13 @@ mod core {
         #[test]
         fn lcg_own() {
             let modulus = (1 << 11) - 1; // 2047
-            let multiplier = 3_i32.pow(5); // 243
+            let multiplier = 3_u64.pow(5); // 243
             let increment = 1;
             let seed = 4;
 
             let mut lcg = LCG::new(modulus, multiplier, increment, seed);
 
-            let mut nums = Vec::<i32>::with_capacity(1000);
+            let mut nums = Vec::<u64>::with_capacity(1000);
             for _ in 0..1000 {
                 let num = lcg.next().expect("Failed generate next number!");
 

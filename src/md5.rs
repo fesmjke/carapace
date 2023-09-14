61e555b8699d5a6c9d489619b608b67c7dcef1f3
fesmjke <fatalerror.sm@gmail.com>
2023-09-14 15:45:15 +0300

    feat: added not implemented md5

---
+++ src/md5.rs
@@ -0,0 +1,17 @@
+pub struct MD5 {
+    // TODO: add md5 parts
+}
+
+impl MD5 {
+    pub fn from(input: &str) -> String {
+        todo!()
+    }
+
+    pub fn new() -> Self {
+        todo!()
+    }
+
+    fn generate(input: &str) -> String {
+        todo!()
+    }
+}

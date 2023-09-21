380f5c709bec5d86edbff6989017e26849f4c7bc
fesmjke <fatalerror.sm@gmail.com>
2023-09-21 17:38:27 +0300

    feat: added uninmplemented rc5 algorithm

---
+++ src/rc5.rs
@@ -0,0 +1,15 @@
+pub struct RC5 {
+    // TODO
+    word_size: u8,
+    rounds: u8,
+    octets: u8,
+}
+
+impl RC5 {
+    pub fn encrypt() {
+        todo!()
+    }
+    pub fn decrypt() {
+        todo!()
+    }
+}

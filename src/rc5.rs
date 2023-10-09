fcf95b016dbd014afc996717974985107b97d97f
fesmjke <fatalerror.sm@gmail.com>
2023-10-09 16:08:06 +0300

    feat: added wordsize and magic constants

--- src/rc5.rs
+++ src/rc5.rs
@@ -1,15 +1,40 @@
+pub enum WordSize {
+    Sixteen,
+    ThirtyTwo,
+    SixtyFour,
+}
+
+impl WordSize {
+    pub fn magic(&self) -> (u64, u64) {
+        match self {
+            WordSize::Sixteen => (0xB7E1, 0x9E37),
+            WordSize::ThirtyTwo => (0xB7E15163, 0x9E3779B9),
+            WordSize::SixtyFour => (0xB7E151628AED2A6B, 0x9E3779B97F4A7C15),
+        }
+    }
+}
+
 pub struct RC5 {
     // TODO
-    word_size: u8,
+    word_size: WordSize,
     rounds: u8,
     octets: u8,
 }
 
 impl RC5 {
-    pub fn encrypt() {
+    pub fn new(word_size: WordSize, rounds: u8, octets: u8) -> Self {
+        Self {
+            word_size,
+            rounds,
+            octets,
+        }
+    }
+
+    pub fn encrypt(&self, plain: &str, key: &str) -> String {
         todo!()
     }
-    pub fn decrypt() {
+
+    pub fn decrypt(&self) {
         todo!()
     }
 }
